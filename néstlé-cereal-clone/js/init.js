3fef7cfc1f74d28a403b4becbf3eb0887b3bd4d0
nguyenit67 <dehungmanh@gmail.com>
2020-11-08 15:35:47 +0700

    update several lines of code

---
+++ néstlé-cereal-clone/js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  // init SLICKjs slider for 
+  // -*BRANDS*- section
+  $(".brands-n-prods__slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
+});
---
+++ trumpchatbot/app.js
@@ -0,0 +1,17 @@
+import * as $ from "F:/NGithubProjects/My-Front-End-Projects/plugins/jquery/jquery-3.5.1.uncompressed.js";
+
+  console.log($);
+  const msgPane = $("#msg");
+  const input = $("#input");
+  
+  function getAnswer(message) {
+    return "fuck you";
+  }
+  
+  function name(params) {
+    
+  }
+
+  
+
+
